2e526c1f0d5ff728be705277158d362b42c25777
Johnathen Harker <tobiaserfurt@gmx.de>
2017-12-31 19:25:16 +0100

    Einlesen der Tanken

--- TankerKoenig.py
+++ TankerKoenig.py
@@ -1,27 +1,135 @@
+import csv 
+import math as M
+import time
+
+def parseDate(date):
+	# parses date (day and time) to day, hour
+	
+	# day zero = 2014-07-01
+	day_zero = 182
+	
+	year = int(date[0:4])
+	month = int(date[5:7])
+	day = int(date[8:10])
+	hour = int(date[11:13])+int(date[19:22])
+	
+	
+	# calculate the day after day_zero
+	normalized_day = (year-2014) *365 + M.floor((year-2014 + 1)/4) -day_zero
+	if month > 1:
+		normalized_day = normalized_day +31
+	if month > 2 and year % 4 == 0:
+		normalized_day = normalized_day +29
+	elif month > 2:
+		normalized_day = normalized_day +28
+	if month > 3:
+		normalized_day = normalized_day +31
+	if month > 4:
+		normalized_day = normalized_day +30
+	if month > 5:
+		normalized_day = normalized_day +31
+	if month > 6:
+		normalized_day = normalized_day +30
+	if month > 7:
+		normalized_day = normalized_day +31
+	if month > 8:
+		normalized_day = normalized_day +31
+	if month > 9:
+		normalized_day = normalized_day +30
+	if month > 10:
+		normalized_day = normalized_day +31
+	if month > 11:
+		normalized_day = normalized_day +30
+		
+	normalized_day = normalized_day + day
+		
+	# last three values probably not needed
+	return (normalized_day, hour)#, year, month, day)
+
 class GasStation:
 	"""
 	represents set of gas stations with ID, position, prizes
 	"""
-	prizingTable
 	
 	
 	def __init__(self):
+		
+		missingData = [5,7,33, 249, 291, 344, 345, 378, 386, 461, 536, 553, 554, 584, 591, 642, 643, 654]
 		print("input gas stations")
 		""" TO DO: 
 		- input IDs and position of gas stations
 		- input historic data
 		"""
+		
+		
+		self.prizingTable = []
+		
+		# read table of gas stations
+		with open('geg. Dateien/Eingabedaten/Tankstellen.csv') as csvfile:
+			readCSV = csv.reader(csvfile, delimiter=';')
+			id = 1
+			for row in readCSV:
+				print("read gas station", id)
+				if id != int(row[0]):
+					print("ERROR: fehlerhafte Tankstellenliste")
+				marke = row[2]
+				nord = float(row[7])
+				sued = float(row[8])
+				
+				# Bedingung muss DRINGEND noch angepasst werden
+				if id %600+1 not in missingData:
+					self.prizingTable.append((id, marke, nord, sued, self.read(1000, id%600+1)))
+				#self.prizingTable.append((id, marke, nord, sued, self.read(600, id)))
+				id = id+1
+		# read historic data
+
 		print ("completed")
 	
-	def findID(self):
+	def findID(self, ID):
 		""" TO DO:
 		- find ID --> return position (and historic prizing data?)
 		"""
 	
-	def read(self, day, ID):
-		""" TO DO:
-		- read historic data for given day and gas station
-		"""
+	def read(self, endDay, ID):
+		
+		# read historic data for given gas station till given day
+		
+		data = []
+		oneDay = []
+		d = 0
+		h = 0
+		prize = 0
+		with open('geg. Dateien/Eingabedaten/Benzinpreise/'+str(ID)+'.csv') as csvfile:
+			readCSV = csv.reader(csvfile, delimiter=';')
+			row = next(readCSV)
+			day, hour = parseDate(row[0])
+			while d < endDay:
+				
+				while day > d or (day == d and hour > h):
+					oneDay.append(prize)
+					if h < 23:
+						h = h+1
+					else:
+						h = 0
+						data.append(day)
+						oneDay = [];
+						d = d+1
+				prize = int(row[1])
+				defaultRow = row
+				row = next(readCSV, defaultRow)
+				if row != defaultRow:
+					day, hour = parseDate(row[0])
+				else:
+					day, hour = endDay+1, 23
+		return data
+				
+		
+		
+	def print(self):
+		# prints all gas stations with positions
+		for station in self.prizingTable:
+			if station[0] < 10:
+				print("ID:", station[0], station[1], "\tN:", station[2], "\tE:", station[3]) 
 		
 class Strategy:
 	"""
@@ -38,8 +146,7 @@ class Route:
 	"""
 	stores a route with (predicted) prizes and fueling amounts
 	"""
-	route
-	capacity
+	
 	
 	def __init__(self, file):
 		# inputs file instantly
@@ -74,7 +181,6 @@ class PrizingForecast:
 	handles files with prize requests
 	"""
 	
-	forecastParameters
 	
 	def read(self, file):
 		""" TO DO:
@@ -103,7 +209,6 @@ class Model:
 	makes predictions for the prize of gas
 	"""
 	
-	data
 	
 	def train(self, data):
 		""" TO DO:
@@ -124,6 +229,7 @@ class Supervisor:
 		""" TO DO:
 		create gas stations, read historic data from files
 		"""
+		self.gasStations = GasStation()
 	
 	def handleRoute(self, file):
 		""" TO DO:
@@ -153,3 +259,16 @@ class Supervisor:
 		- call right function at the right time
 		- control user
 		"""
+		input('hi')
+
+		
+		
+t1 = time.clock()		
+		
+S = Supervisor()
+S.handleHandle()
+
+t2 = time.clock()
+
+dt = t1-t2
+print("Zeit:", dt)
