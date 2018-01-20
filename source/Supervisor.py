e465ec6475c7e141bbaf6e704bd6391fc7c6b67e
Tilman Hinnerichs <ti.hinnerichs+github@gmail.com>
2018-01-20 18:47:32 +0100

    Added logic to supervisor class

--- source/Supervisor.py
+++ source/Supervisor.py
@@ -3,6 +3,8 @@ from Input import *
 from Strategy import Strategy
 from Model import Model
 
+import os.path
+
 
 class Supervisor:
 	"""
@@ -26,7 +28,7 @@ class Supervisor:
 		""" TO DO:
 		create gas stations, read historic data from files
 		"""
-		self.gasStations = GasStation()
+		self.gasStation = GasStation()
 
 	def handleRoute(self, file):
 		""" TO DO:
@@ -38,6 +40,29 @@ class Supervisor:
 		- call appendAmount(Strategys solution)
 		- write solution to file
 		"""
+
+		route = Route()
+		route.read(file)
+		model = Model()
+		model.train(self.gasStation, route.route[0][0][0]-1, 500)
+
+		print("Starting to calculate prize for each station on route")
+		prizeList = []
+		for element in route.route:
+			(date, hour), ID, prize, amount = element
+			prizeList.append(model.forecast(ID, date, hour, self.gasStation))
+
+		route.appendPrize(prizeList)
+		print("Finished calculating")
+
+		#calculating best tanking strategy
+		strategy = Strategy(self.gasStation)
+		strategy.calculate(route)
+
+		#writing route
+		route.write(file)
+
+
 
 	def handlePrizingForecast(self, file):
 		""" TO DO:
@@ -51,15 +76,60 @@ class Supervisor:
 		- write solution to file
 		"""
 
+		prizingForecast = PrizingForecast()
+		prizingForecast.read(file)
+
+		model = Model()
+
+		print("Starting to calculate forecasts")
+		prizeList = []
+		for element in prizingForecast.forecastParameter:
+			((lastKnownDate,lastKnownHour),(forecastDate, forecastHour)), ID, prize = element
+			model.train(self.gasStation, lastKnownDate, 500)
+			prizeList.append(model.forecast(ID, forecastDate, forecastHour, self.gasStation))
+		print("Finished forecasts")
+
+		prizingForecast.appendPrize(prizeList)
+
+		prizingForecast.write(file)
+
+
+
+
 	def handleHandle(self):
 		""" TO DO:
 		- call right function at the right time
 		- control user
 		"""
+		"""
 		m = Model()
-		m.train(self.gasStations, 700, 500)
+		m.train(self.gasStation, 700, 500)
 		#for i in [1,2,3,4,5,10,15, 20, 25, 30]:
 		for i in [30]:
-			print(m.forecast(6, 400+i, 4, self.gasStations))
-			print(self.gasStations.findID(6)[4][400+i][4])
-		m.evaluate(self.gasStations)
+			print(m.forecast(6, 400+i, 4, self.gasStation))
+			print(self.gasStation.findID(6)[4][400+i][4])
+		m.evaluate(self.gasStation)
+		"""
+
+		with open("config", encoding='utf-8') as file:
+			for line in file:
+				if line.__contains__("#"):
+					pass
+				else:
+					listList = line.split(";")
+					command = listList[0]
+					path = listList[1]
+
+					if command == "route" or command == "Route":
+						if os.path.isfile(path):
+							self.handleRoute(listList[1])
+						else:
+							print("Invalid path", path)
+					elif command == "forecast" or command == "Forecast":
+
+						if os.path.isfile(path):
+							self.handlePrizingForecast(listList[1])
+						else:
+							print("Invalid path", path)
+					else:
+						print("Invalid command", command)
